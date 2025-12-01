436c82ecf930cb1d8c76b7d9501aa73ef56c5a68
TimN1987 <timothy.naish@gmail.com>
2025-12-01 08:42:53 +0000

    Corrected directory paths.

---
+++ CSharp/AoC2025/Days/Day1.cs
@@ -0,0 +1,13 @@
+namespace AoC2025.Days;
+
+public static class Day1
+{
+    
+
+    // Data methods
+
+    private string[] GetData()
+    {
+        return File.ReadAllLines("../../../Data/2025/day1.txt");
+    }
+}
