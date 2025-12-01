9a5129a6e4d4276830d00093af951e673cd3d080
TimN1987 <timothy.naish@gmail.com>
2025-12-01 13:18:26 +0000

    Added day 1 part 1 in Python.

---
+++ Python/AoC2025/days/day1.py
@@ -0,0 +1,31 @@
+class Day1:
+    def __init__(self):
+        with open("../../Data/2025/day1.txt") as f:
+            self.data = f.readlines()
+
+    def parse_data(self):
+        rotations = []
+        for line in self.data:
+            if line[0] == 'R':
+                rotations.append(int(line[1:]))
+            elif line[0] == 'L':
+                rotations.append(0 - int(line[1:]))
+        return rotations
+
+    def part_one(self):
+        count = 0
+        dial = 50
+        rotations = self.parse_data()
+        for rotation in rotations:
+            dial = (dial + rotation + 100) % 100
+            if dial == 0:
+                count += 1
+        return count
+    
+    def part_two(self):
+        return 0
+    
+    def print_results(self):
+        print("Day 1:")
+        print(f"Part 1: {self.part_one()}")
+        print(f"Part 2: {self.part_two()}")
---
+++ Python/AoC2025/main.py
@@ -0,0 +1,5 @@
+from days.day1 import Day1
+
+# Day 1
+day1 = Day1()
+day1.print_results()
