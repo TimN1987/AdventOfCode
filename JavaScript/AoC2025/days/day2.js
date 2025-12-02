ee1bdbfbd0877141d1b0e2681dd8dc5136ffe4f0
TimN1987 <timothy.naish@gmail.com>
2025-12-02 14:02:09 +0000

    Updated JS solution with Regex.

--- JavaScript/AoC2025/days/day2.js
+++ JavaScript/AoC2025/days/day2.js
@@ -1,24 +1,48 @@
 import fs from "fs"
-import path from pathlib
+import path from "path"
 
-export default class Day1 {
+export default class Day2 {
     constructor() {
-        const filePath = path.resolve("../../Data/2025/day1.txt");
+        const filePath = path.resolve("../../Data/2025/day2.txt");
         const data = fs.readFileSync(filePath, "utf-8");
-        this.lines = data.trim().split("\n");
+        this.lines = data.trim().split(",");
+        this.doubleRegex = /^(\d+)\1$/;
+        this.multipleRegex = /^(\d+)\1+$/;
     }
 
     printResults() {
-        console.log("Day 1:");
+        console.log("Day 2:");
         console.log(`Part 1: ${this.part1()}`);
         console.log(`Part 2: ${this.part2()}`);
     }
 
     part1() {
+        let total = 0;
+        for (const line of this.lines) {
+            const boundaries = line.split("-");
+            const start = parseInt(boundaries[0]);
+            const end = parseInt(boundaries[1]);
 
+            for (let i = start; i <= end; i++) {
+                if (this.doubleRegex.test(i.toString()))
+                    total += i;
+            }
+        }
+        return total;
     }
 
     part2() {
-        
+        let total = 0;
+        for (const line of this.lines) {
+            const boundaries = line.split("-");
+            const start = parseInt(boundaries[0]);
+            const end = parseInt(boundaries[1]);
+
+            for (let i = start; i <= end; i++) {
+                if (this.multipleRegex.test(i.toString()))
+                    total += i;
+            }
+        }
+        return total;
     }
 }
--- JavaScript/AoC2025/main.js
+++ JavaScript/AoC2025/main.js
@@ -1,4 +1,8 @@
 import Day1 from "./days/day1.js"
+import Day2 from "./days/day2.js"
 
 const day1 = new Day1();
 day1.printResults();
+
+const day2 = new Day2();
+day2.printResults();
