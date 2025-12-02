3acd1edb2719d0b9c90f8fd8a6c46e04db6af95b
TimN1987 <timothy.naish@gmail.com>
2025-12-02 10:53:54 +0000

    Day2 part1 solution.

--- CSharp/AoC2025/Days/Day2.cs
+++ CSharp/AoC2025/Days/Day2.cs
@@ -2,12 +2,117 @@ namespace AoC2025.Days;
 
 public static class Day2
 {
+    public static long PartOne()
+    {
+        long total = 0;
 
+        foreach (string[] s in ParseData())
+        {
+            total += CalculateRepeatingNumbers(s[0], s[1]);
+        }
+
+        return total;
+    }
+
+    // Helper methods
+
+    private static long CalculateRepeatingNumbers(string start, string end)
+    {
+        long numStart = Convert.ToInt64(start);
+        long numEnd = Convert.ToInt64(end);
+        int startLength = start.Length;
+        int endLength = end.Length;
+
+        // Odd length numbers cannot repeat - return 0 if no even length numbers.
+        if (startLength == endLength && startLength % 2 == 1)
+            return 0;
+
+        // If the start length is odd, increase the number to the next even length number.
+        // e.g. 758 is length 3 => 10^3 = 1000
+        if (startLength % 2 == 1)
+        {
+            numStart = (long)Math.Pow(10, startLength);
+            startLength++;
+            start = Convert.ToString(numStart);
+        }
+
+        // If the end length is odd, decrease the number the the previous even length number.
+        // e.g. 948 is length 3 => 10^2 - 1 = 99
+        if (endLength % 2 == 1)
+        {
+            endLength--;
+            numEnd = (long)Math.Pow(10, endLength) - 1;
+            end = Convert.ToString(numEnd);
+        }
+
+        if (numStart > numEnd)
+            return 0;
+
+        // If all the numbers have the same length, then we only need to check how many start numbers will be reapeated.
+        // e.g. 23 -> 84 => 33, 44, 55, 66, 77 - first is 3, last is 7 i.e. 5 options
+        // All lengths are now confirmed as even.
+        if (startLength == endLength)
+        {
+            // Find the patterns to be repeated.
+            int patternLength = startLength / 2;
+            long startPattern = Convert.ToInt64(start[..patternLength]);
+            long startRemainder = Convert.ToInt64(start[patternLength..]);
+            long endPattern = Convert.ToInt64(end[..patternLength]);
+            long endRemainder = Convert.ToInt64(end[patternLength..]);
+
+            // Check if the first and last repeating patterns are in the range.
+            long startValue = startRemainder > startPattern ? startPattern + 1 : startPattern;
+            long endValue = endRemainder >= endPattern ? endPattern : endPattern - 1;
+           
+            // Use start and end values to calculate the total of the repeated patterns.
+            // Use triangle number formula to get the total - triangle(end) - triangle(start - 1).
+            // As this is repeated, it needs to have 1 * total + 10^length * total
+            long patternTotal = (endValue * (endValue + 1) / 2) - (startValue * (startValue - 1) / 2);
+            long result = patternTotal + (long)Math.Pow(10, patternLength) * patternTotal;
+            return result;
+        }
+
+        // If the start length and end length are not the same, then there are multiple even length ranges to consider.
+        // Split the larger range into smaller valid ranges and use recursion.
+        // e.g. lengths 2 and 4 => (startValue, 99), (1000, endValue)
+        List<(string, string)> ranges = new();
+
+        long tempEnd = (long)Math.Pow(10, startLength) - 1; // e.g. length 2 => 99
+        ranges.Add((start, Convert.ToString(tempEnd)));
+        startLength += 2;
+
+        // Find any middle even length ranges where the full range is included.
+        while (startLength < endLength)
+        {
+            // e.g. length 2 => newStart = 10, newEnd = 99
+            long newStart = (long)Math.Pow(10, startLength - 1);
+            long newEnd = (long)Math.Pow(10, startLength) - 1;
+        }
+
+        long tempStart = (long)Math.Pow(10, endLength - 1); //e.g. length 4 => 1000
+        ranges.Add((Convert.ToString(tempStart), end));
+
+        long total = 0;
+
+        foreach (var (s, e) in ranges)
+        {
+            total += CalculateRepeatingNumbers(s, e);
+        }
+
+        return total;
+    }
     
     // Data methods
 
-    private static string[] GetData()
+    private static string GetData()
     {
-        return File.ReadAllLines("../../Data/2025/day1.txt");
+        return File.ReadAllText("../../Data/2025/day2.txt");
+    }
+
+    private static IEnumerable<string[]> ParseData()
+    {
+        string[] data = GetData().Split(',');
+        
+        return data.Select(d => d.Split('-'));
     }
 }
--- CSharp/AoC2025/Program.cs
+++ CSharp/AoC2025/Program.cs
@@ -1,7 +1,11 @@
 ﻿using AoC2025.Days;
 
 int day1Part1Result = Day1.Part1();
-int day2Part2Result = Day1.Part2();
+int day1Part2Result = Day1.Part2();
 int bruteForce = Day1.Part2B();
 
-Console.WriteLine($"Day 1:\nPart 1: {day1Part1Result}\nPart2: {day2Part2Result}\nBrute force: {bruteForce}");
+Console.WriteLine($"Day 1:\nPart 1: {day1Part1Result}\nPart2: {day1Part2Result}\nBrute force: {bruteForce}");
+
+long day2Part1result = Day2.PartOne();
+
+Console.WriteLine($"Day 2:\nPart 1: {day2Part1result}");
