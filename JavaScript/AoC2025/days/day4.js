5cbdb08062f918225a8590b314f04929e1e51b5e
TimN1987 <timothy.naish@gmail.com>
2025-12-04 12:46:08 +0000

    Added JS solution for both parts day 4.

--- JavaScript/AoC2025/days/day4.js
+++ JavaScript/AoC2025/days/day4.js
@@ -3,8 +3,12 @@ import path from "path"
 
 export default class Day4 {
     constructor() {
-        const filePath = path.resolve("../../Data/2025/day3.txt");
+        const filePath = path.resolve("../../Data/2025/day4.txt");
         const data = fs.readFileSync(filePath, "utf-8");
+        this.lines = data.split('\n').map(line => line.trimEnd());
+        this.height = this.lines.length;
+        this.width = this.lines[0].length;
+        this.grid = this.padGrid();
     }
 
     printResults() {
@@ -14,8 +18,55 @@ export default class Day4 {
     }
 
     part1() {
+        let result = 0;
+        for (let i = 1; i < this.height + 1; i++) {
+            for (let j = 1; j < this.width + 1; j++) {
+                if (this.grid[i][j] === '@' && this.countNeighbours(i, j) < 4) {
+                    result++;
+                }
+            }
+        }
+        return result;
     }
 
     part2() {
+        let result = 0;
+        while (true) {
+            let total = 0;
+            for (let i = 1; i < this.height + 1; i++) {
+                for (let j = 1; j < this.width + 1; j++) {
+                    if (this.grid[i][j] === '@' && this.countNeighbours(i, j) < 4) {
+                        total++;
+                        this.grid[i][j] = '.';
+                    }
+                }
+            }
+            result += total;
+            if (total === 0)
+                break;
+        }
+        return result;
+    }
+
+    padGrid() {
+        const array = []
+        const emptyLine = Array(this.width + 2).fill('.');
+        array.push([...emptyLine]);
+        for (const line of this.lines) {
+            array.push(("." + line + ".").split(""));
+        }
+        array.push([...emptyLine]);
+        return array;
+    }
+
+    countNeighbours(row, col) {
+        let total = 0;
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                if (this.grid[row + i][col + j] == '@')
+                    total++;
+            }
+        }
+        return total - 1; // Ignore the @ in row + 0, col + 0
     }
 }
