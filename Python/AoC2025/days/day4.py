25766350c49df7a450a4fa2ba8883a885814c8ec
TimN1987 <timothy.naish@gmail.com>
2025-12-04 06:33:57 +0000

    Completed parts 1 and 2 in Python.

--- Python/AoC2025/days/day4.py
+++ Python/AoC2025/days/day4.py
@@ -1,21 +1,33 @@
 class Day4:
     def __init__(self):
         with open("../../Data/2025/day4.txt") as f:
-            self.data = f.readlines()
+            self.data = [line.rstrip('\n') for line in f]
         self.padded_data = self.pad_data()
         self.ADJACENT_DELTAS = [-1, 0, 1]
 
     def part_one(self):
         result = 0
         rows, cols = len(self.padded_data), len(self.padded_data[0])
-        for i in range(rows):
-            for j in range(cols):
+        for i in range(1, rows -1):
+            for j in range(1, cols - 1):
                 if self.padded_data[i][j] == '@':
                     result += 1 if self.count_adjacent(i, j) < 4 else 0
         return result
     
     def part_two(self):
-        pass
+        result = 0
+        rows, cols = len(self.padded_data), len(self.padded_data[0])
+        while True:
+            total = 0
+            for i in range(1, rows -1):
+                for j in range(1, cols - 1):
+                    if self.padded_data[i][j] == '@' and self.count_adjacent(i, j) < 4:
+                        total += 1
+                        self.padded_data[i][j] = '.'
+            result += total
+            if total == 0:
+                break
+        return result
 
     def count_adjacent(self, row, col):
         total = 0 if self.padded_data[row][col] == '.' else -1
@@ -28,10 +40,10 @@ class Day4:
     def pad_data(self):
         new_grid = []
         length = len(self.data[0])
-        new_grid.append(" " * (length + 2))
+        new_grid.append(["."] * (length + 2))
         for line in self.data:
-            new_grid.append(" " + line + " ")
-        new_grid.append(" " * (length + 2))
+            new_grid.append(["."] + list(line) + ["."])
+        new_grid.append(["."] * (length + 2))
         return new_grid
 
     def print_solution(self):
