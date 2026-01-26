4dabb9f2484d3dacbe4ca639841712e3afb4ad4e
TimN1987 <timothy.naish@gmail.com>
2026-01-26 22:20:07 +0000

    Added day methods to main.py

--- Python/AOC2015/main.py
+++ Python/AOC2015/main.py
@@ -2,6 +2,8 @@ from days.day1 import Day1
 from days.day2 import Day2
 from days.day3 import Day3
 from days.day4 import Day4
+from days.day5 import Day5
+from days.day6 import Day6
 
 # Day 1
 day1 = Day1()
@@ -18,3 +20,11 @@ day3.deliver()
 # Day 4
 day4 = Day4()
 day4.find_answers()
+
+# Day 5
+day5 = Day5()
+day5.check_niceness()
+
+# Day 6
+day6 = Day6()
+day6.run_light_display()
