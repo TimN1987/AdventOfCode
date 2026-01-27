bcce4444f262117e50ad1665be74d92498405e9f
TimN1987 <timothy.naish@gmail.com>
2026-01-27 14:46:52 +0000

    Added JS folder for 2015 and completed day 1 solution.

---
+++ JavaScript/AOC2015/days/day1.js
@@ -0,0 +1,30 @@
+import fs from "fs"
+import path from "path"
+
+export default class Day1 {
+    constructor() {
+        const filePath = path.resolve("../../Data/2015/day1.txt");
+        this.data = fs.readFileSync(filePath, "utf-8");
+        this.floor = 1;
+        this.basementEntered = false;
+        this.basementTime = 0;
+    }
+
+    moveLift() {
+        for (const d of this.data) {
+            if (d === '(') {
+                this.floor++;
+            } else {
+                this.floor--;
+            }
+
+            if (!this.basementEntered && this.floor === -1) {
+                this.basementEntered = true;
+            } else if (!this.basementEntered) {
+                this.basementTime++;
+            }
+        }
+
+        console.log(`The final floor was ${this.floor}, and the basement was entered at ${this.basementTime}.`);
+    }
+}
---
+++ JavaScript/AOC2015/index.js
@@ -0,0 +1,5 @@
+import Day1 from "./days/day1.js"
+
+// Day 1
+const day1 = new Day1();
+day1.moveLift();
