d4c99cafc90c43ab33ebe100ee06875bc952ac95
TimN1987 <timothy.naish@gmail.com>
2026-01-27 19:28:41 +0000

    Added timing in ms for JS solutions to index.js

--- JavaScript/AOC2015/index.js
+++ JavaScript/AOC2015/index.js
@@ -3,8 +3,14 @@ import Day2 from "./days/day2.js"
 
 // Day 1
 const day1 = new Day1();
+let start = process.hrtime.bigint();
 day1.moveLift();
+let end = process.hrtime.bigint();
+console.log(`Day 1 run time = ${(Number(end - start) / 1_000_000).toFixed(4)} ms.`);
 
 // Day 2
 const day2 = new Day2();
+start = process.hrtime.bigint();
 day2.orderStock();
+end = process.hrtime.bigint();
+console.log(`Day 2 run time = ${(Number(end - start) / 1_000_000).toFixed(4)} ms.`);
