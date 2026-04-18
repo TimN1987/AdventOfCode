9ed7d22228a99967d41f7a4625735a047e2ed00e
TimN1987 <timothy.naish@gmail.com>
2026-04-18 22:06:48 +0100

    Completed AOC2018 Day2 Part2 C#.

--- CSharp/AOC2018/Days/Day2.cs
+++ CSharp/AOC2018/Days/Day2.cs
@@ -1,6 +1,77 @@
+using System.Text;
+
 namespace AOC2018.Days;
 
 public class Day2
 {
-    
+    private string[] _data = File.ReadAllLines("../../Data/2018/day2.txt");
+
+    public int Part1()
+    {
+        int twoMatchingCount = 0, threeMatchingCount = 0;
+        for (int i = 0; i < _data.Length; ++i)
+        {
+            CheckRepeatedLetters(_data[i], out bool containsTwo, out bool containsThree);
+            twoMatchingCount += containsTwo ? 1 : 0;
+            threeMatchingCount += containsThree ? 1 : 0;
+        }
+        return twoMatchingCount * threeMatchingCount;
+    }
+
+    public string Part2()
+    {
+        for (int i = 0; i < _data.Length - 1; ++i)
+        {
+            for (int j = i + 1; j < _data.Length; ++j)
+            {
+                if (HasOneCharacterDifference(_data[i], _data[j]))
+                {
+                    StringBuilder output = new();
+                    for (int k = 0; k < _data[i].Length; ++k)
+                    {
+                        if (_data[i][k] == _data[j][k])
+                            output.Append(_data[i][k]);
+                    }
+                    return output.ToString();
+                }
+            }
+        }
+        return "";
+    }
+
+    private static void CheckRepeatedLetters(string id, out bool containsTwo, out bool containsThree)
+    {
+        containsTwo = false;
+        containsThree = false;
+
+        int[] letters = new int[26];
+
+        for (int i = 0; i < id.Length; ++i)
+        {
+            letters[id[i] - 'a']++;
+        }
+        for (int i = 0; i < 26; ++i)
+        {
+            if (letters[i] == 2)
+                containsTwo = true;
+            if (letters[i] == 3)
+                containsThree = true;
+            if (containsTwo && containsThree)
+                return;
+        }
+    }
+
+    private static bool HasOneCharacterDifference(string s1, string s2)
+    {
+        bool differenceFound = false;
+        for (int i = 0; i < s1.Length; ++i)
+        {
+            if (s1[i] == s2[i])
+                continue;
+            if (differenceFound)
+                return false;
+            differenceFound = true;
+        }
+        return differenceFound;
+    }
 }
--- CSharp/AOC2018/Program.cs
+++ CSharp/AOC2018/Program.cs
@@ -7,3 +7,10 @@ Day1 day1 = new Day1();
 Console.WriteLine("Day 1 solutions:");
 Console.WriteLine("The final frequency is: {0}", day1.Part1());
 Console.WriteLine("The repeated frequency is: {0}", day1.Part2());
+
+// Day 2
+
+Day2 day2 = new Day2();
+Console.WriteLine("Day 2 solutions:");
+Console.WriteLine("The checksum is: {0}", day2.Part1());
+Console.WriteLine("The common letters are: {0}", day2.Part2());
