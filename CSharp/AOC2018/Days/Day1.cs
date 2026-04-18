68c25a99974d160c77e5525b2fb87ce37a60c175
TimN1987 <timothy.naish@gmail.com>
2026-04-18 21:19:27 +0100

    2018 Day1 Part1 in C# uploaded.

--- CSharp/AOC2018/Days/Day1.cs
+++ CSharp/AOC2018/Days/Day1.cs
@@ -2,5 +2,17 @@ namespace AOC2018.Days;
 
 public class Day1
 {
-    
+    private string[] _data = GetData();
+
+    public int Part1()
+    {
+        int frequency = 0;
+        for (int i = 0; i < _data.Length; ++i)
+        {
+            frequency += _data[i][0] == '+' ? int.Parse(_data[i][1..]) : - int.Parse(_data[i][1..]);
+        }
+        return frequency;
+    }
+
+    private static string[] GetData() => File.ReadAllLines("../../Data/2018/day1.txt");
 }
--- CSharp/AOC2018/Program.cs
+++ CSharp/AOC2018/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using AOC2018;
+using AOC2018.Days;
+
+// Day 1
+
+Day1 day1 = new Day1();
+Console.WriteLine(day1.Part1());
